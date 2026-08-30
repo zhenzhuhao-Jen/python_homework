87d61ad8fd6bcd54d8020fa15bc0c732c34323ca
Zhenzhu Hao <zhenzhu@Zhenzhus-MacBook-Air.local>
2026-08-29 20:56:12 -0400

    changes made

--- assignment8/get_books.py
+++ assignment8/get_books.py
@@ -1,8 +1,7 @@
 #Task 3
 
 import pandas as pd
-import json
-import csv
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,6 +10,8 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 try:
     driver.get("https://durhamcounty.bibliocommons.com/v2/search?query=learning%20spanish&searchType=smart")
+    # Wait for the page/results to load
+    driver.implicitly_wait(5)
     print(driver.title)
 
     #find li elements
@@ -24,8 +25,10 @@ try:
         book_title = li.find_element(By.CSS_SELECTOR, 'span.title-content').text.strip()
         
         #entries that contain the authors of the book, and get the text for each, more than one author, you want to join the author names with a semicolon ; between each
-        book_author = li.find_element(By.CSS_SELECTOR, 'a.author-link').text.strip()
-        #author_text = "; ".join(authors)
+        authors = li.find_elements(By.CSS_SELECTOR, 'a.author-link')
+        
+        book_author= "; ".join([author.text for author in authors])
+
         #find the div that contains the format and the year, and then you find the span entry within it that contains this information.  You get that text too
         format_div = li.find_element(By.CSS_SELECTOR, '.cp-format-info')
         format_year = format_div.find_element(By.CSS_SELECTOR, 'span.cp-screen-reader-message').text.strip()
@@ -38,7 +41,15 @@ try:
     #Create a DataFrame from this list of dicts.  Print the DataFrame
     book_info = pd.DataFrame(results)
     print(book_info)
+    print(book_info.head())
 
+    # Task 4 Write out the Data
+    import json
+    import csv
+    book_info.to_csv('./assignment8/get_books.csv', index=False)
+
+    with open('./assignment8/get_books.json', 'w') as json_file:
+        json.dump(results, json_file, indent=4)
 
 except Exception as e:
     print(f"Error: {type(e).__name__}: {e}")
@@ -46,9 +57,8 @@ except Exception as e:
 finally:
      driver.quit()
 
-# Task 4 Write out the Data
-book_info.to_csv('./assignment8/get_books.csv', index=False)
-with open('./assignment8/get_books.json', 'w') as json_file:
-    json.dump(results, json_file, indent=4)
+
+
+
 
 
