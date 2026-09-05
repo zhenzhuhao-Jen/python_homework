647d95bcb22ef0f7821bd57af06ca4fef334733a
Zhenzhu Hao <zhenzhu@zhenzhus-air.mynetworksettings.com>
2026-09-04 23:28:54 -0400

    assignment9 completed

---
+++ assignment9/sql_intro.py
@@ -0,0 +1,120 @@
+#Task1
+import sqlite3
+
+#define function to populate tables
+
+def add_publisher(cursor, name):
+    try:
+        cursor.execute("INSERT INTO Publishers (name) VALUES (?)", [name])
+    except sqlite3.IntegrityError:
+        print(f"{name} is already in the database.")
+
+def add_magazine(cursor, name, publisher_id):
+    try:
+        cursor.execute("INSERT INTO Magazines (name, publisher_id) VALUES (?,?)", (name, publisher_id))
+    except sqlite3.IntegrityError:
+        print(f"{name} is already in the database.")
+
+def add_subscriber(cursor, name, address):
+    try:
+        cursor.execute("SELECT * FROM Subscribers WHERE name = ? AND address = ?", (name, address))
+        results = cursor.fetchall()
+        if len(results) > 0:
+            print(f"There is already a subscriber {name} at address {address}.")
+            return
+        cursor.execute("INSERT INTO Subscribers (name, address) VALUES (?,?)", (name, address))
+    except sqlite3.IntegrityError:
+        print("error adding subscriber")
+
+def add_subscription(cursor, subscriber_id, magazine_id, expiration_date):
+    try:
+        cursor.execute("INSERT INTO Subscriptions (subscriber_id, magazine_id, expiration_date) VALUES (?,?,?)", (subscriber_id, magazine_id, expiration_date))
+    except sqlite3.IntegrityError:
+        print("error adding subscription")
+    
+# Connect to a new SQLite database
+with  sqlite3.connect("../db/magazines.db") as conn:
+    conn.execute("PRAGMA foreign_keys = 1")
+    cursor = conn.cursor()
+    try:
+        # Task2
+        # Create tables
+        #publishers: publisher_id;name
+        #magazines: magazine_id; name, publisher_id
+        #subscribers: subscriber_id; name; address;
+        #subscriptions: subscripotion_id; subscriber_id; magazine_id; expiration_date (a string) 
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Publishers (
+            publisher_id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL UNIQUE
+        )
+        """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Magazines (
+            magazine_id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL UNIQUE,
+            publisher_id INTEGER,
+            FOREIGN KEY (publisher_id) REFERENCES Publishers (publisher_id)
+        )
+        """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Subscribers (
+            subscriber_id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            address TEXT NOT NULL
+        )
+        """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Subscriptions (
+            subscription_id INTEGER PRIMARY KEY,
+            subscriber_id INTEGER,
+            magazine_id INTEGER,
+            expiration_date TEXT NOT NULL,
+            FOREIGN KEY (subscriber_id) REFERENCES Subscribers (subscriber_id),
+            FOREIGN KEY (magazine_id) REFERENCES Magazines (magazine_id)   
+        )
+        """)
+
+        add_publisher(cursor, "Time USA, LLC")
+        add_publisher(cursor, "Hearst Magazines Inc.")
+        add_publisher(cursor, "Dotdash Meredith")
+        add_magazine(cursor, "Time", 1)
+        add_magazine(cursor, "Cosmopolitan", 2)
+        add_magazine(cursor, "People", 3)
+        add_subscriber(cursor, "John", "12345 gold fish dr, Jersey City, NJ")
+        add_subscriber(cursor, "William", "67890 green turtle ln, Vienna, VA")
+        add_subscriber(cursor, "Jane", "45636 yellow bird ct, Miami, FL")
+        add_subscription(cursor, 1, 1, "2027-10-02")
+        add_subscription(cursor, 2, 2, "2027-11-12")
+        add_subscription(cursor, 3, 3, "2028-03-05")
+
+        conn.commit()
+    except sqlite3.IntegrityError:
+        print("ERROR")
+    #conn.close()
+
+#Task 4 
+
+    #retrieve all information from the subscribers table
+
+    cursor.execute("SELECT * FROM Subscribers")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+    #retrieve all magazines sorted by name
+
+    cursor.execute("SELECT name FROM Magazines ORDER BY name")
+    result = cursor.fetchall()
+    for row in result:
+            print(row)
+
+    #find magazines for a particular publisher by using JOIN
+
+    cursor.execute("SELECT Magazines.name  FROM Magazines JOIN Publishers ON Magazines.publisher_id = Publishers.publisher_id WHERE Publishers.name = 'Dotdash Meredith'")
+    result = cursor.fetchall()
+    for row in result:
+            print(row)
---
+++ assignment9/sql_intro_2.py
@@ -0,0 +1,25 @@
+#Task5
+
+import pandas as pd
+import sqlite3
+
+with sqlite3.connect("../db/lesson.db") as conn:
+    #line_item_id, quantity, product_id, product_name, and price
+    sql_statement = """SELECT li.line_item_id, li.quantity, p.product_id, p.product_name, p.price FROM line_items li JOIN products p ON li.product_id = p.product_id;"""
+    df = pd.read_sql_query(sql_statement, conn)
+    print(df)
+    print(f'first 5 rows: \n{df.head()}')
+    #add column 'Total'
+    df['total'] = df['quantity'] * df['price']
+    print(f'first 5 rows after add column total: \n{df.head()}')
+
+    #group dataframe
+    df = df.groupby('product_id').agg({'line_item_id': 'count', 'total': 'sum', 'product_name': 'first'})
+    print(f'first 5 rows after group dataframe: \n{df.head()}')
+
+    #Sort the DataFrame by the product_name column.
+    df = df.sort_values(by='product_name')
+
+    #write this dataframe to csv file
+    df.to_csv('order_summary.csv')
+    
