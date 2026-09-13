bc71113ebcb41da1f0e6981acb9fd9107d4db6a1
Zhenzhu Hao <zhenzhu@Zhenzhus-MacBook-Air.local>
2026-09-13 14:43:44 -0400

    changes made

--- assignment10/advanced_sql.py
+++ assignment10/advanced_sql.py
@@ -39,15 +39,14 @@ with sqlite3.connect("../db/lesson.db") as conn:
     SELECT c.customer_name, AVG(subquery.total_price) AS average_total_price
     FROM customers c 
     LEFT JOIN (
-        SELECT c.customer_id AS customer_id_b, SUM(p.price * li.quantity) AS total_price
-        FROM customers c
-        JOIN orders o ON  c.customer_id = o.customer_id
+        SELECT o.customer_id AS customer_id_b, SUM(p.price * li.quantity) AS total_price
+        FROM orders o
         JOIN line_items li ON o.order_id = li.order_id
         JOIN products p ON p.product_id = li.product_id
-        GROUP BY o.order_id
+        GROUP BY o.order_id, o.customer_id
         )  AS subquery
     ON c.customer_id = subquery.customer_id_b 
-    GROUP BY c.customer_id;
+    GROUP BY c.customer_id, c.customer_name;
     """
 
     cursor.execute(query)
@@ -56,32 +55,39 @@ with sqlite3.connect("../db/lesson.db") as conn:
 
 
     #Task 3: An Insert Transaction Based on Data
-    # Then, using a SELECT with a JOIN, print out the list of line_item_ids for the order 
-    # along with the quantity and product name for each.
+    
 
     try:
-        cursor.execute("""
-        INSERT INTO orders (customer_id, employee_id, date)
+        
+        
+        # find customer_id
+        cursor.execute("SELECT customer_id FROM customers WHERE customer_name = 'Perez and Sons'")
+        customer_id = cursor.fetchone()[0]
+        #find employee_id
+        cursor.execute("SELECT employee_id FROM employees WHERE first_name = 'Miranda' AND last_name = 'Harris'")
+        employee_id = cursor.fetchone()[0]
 
-        SELECT c.customer_id, e.employee_id, CURRENT_DATE AS date
-        FROM customers c, employees e
-        WHERE c.customer_name = 'Perez and Sons'
-            AND e.first_name = 'Miranda' AND e.last_name = 'Harris'
-        RETURNING order_id;
-        """)
+        #find five cheapest products
+        cursor.execute("SELECT product_id FROM products ORDER BY price ASC LIMIT 5")
+        product_rows = cursor.fetchall()
+        product_ids = [row[0] for row in product_rows]
+
+        #begin traction:
+        conn.execute("BEGIN")
+        #insert step 1
+        cursor.execute("""INSERT INTO orders (customer_id, employee_id, date) VALUES (?, ?, CURRENT_DATE) RETURNING order_id""", (customer_id, employee_id))
         new_order_id = cursor.fetchone()[0]
-        cursor.execute("""
-        INSERT INTO line_items (order_id, product_id, quantity)
-        SELECT ?, p.product_id, 10 AS quantity
-        FROM products p
-        ORDER BY p.price ASC
-        LIMIT 5;
-        """, (new_order_id,))
+        #insert step 2
+        for product_id in product_ids:
+            cursor.execute(
+            "INSERT INTO line_items (order_id, product_id, quantity) VALUES (?, ?, ?)",
+            (new_order_id, product_id, 10)
+            )
         conn.commit()
     except Exception as e:
         conn.rollback()
         print("error:", e)
-    
+
     print( "result after adding the new order:")
     query = """
     SELECT li.line_item_id, li.quantity, p.product_name
@@ -90,19 +96,29 @@ with sqlite3.connect("../db/lesson.db") as conn:
     WHERE li.order_id = ?
     """
     cursor.execute(query,(new_order_id,))
-    print(cursor.fetchall())
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
 
-    #Task 4: Aggregation with HAVING
+
+
+        
+
+
+    #Task 4
     
     
     query = """
     SELECT e.employee_id, e.first_name, e.last_name, COUNT(o.order_id) AS count_of_orders
     FROM employees AS e
     JOIN orders AS o ON e.employee_id = o.employee_id
-    GROUP BY o.employee_id
+    GROUP BY e.employee_id, e.first_name, e.last_name
     HAVING COUNT(o.order_id) > 5;
     """
     cursor.execute(query)
-    print(cursor.fetchall())
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    
 
